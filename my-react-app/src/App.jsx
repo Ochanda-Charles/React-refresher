dd388e59499bef54c4659c936549ca34864a76f9
Ochanda Charles <ochanda.charles.16@gmail.com>
2025-08-05 17:38:35 +0300

    learn't how to receive data from an API

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Jokes from '../components/jokes.jsx'
-
+import data from '../components/data.js'
+import Entry from '../components/entry.jsx'
 
 // function App() {
 //   const [count, setCount] = useState(0)
@@ -65,34 +66,74 @@ import Jokes from '../components/jokes.jsx'
 export default App */}
 
 
-import Header from "../components/header.jsx"
-import Entry from "../components/entry.jsx"
+// import Header from "../components/header.jsx"
+// import Entry from "../components/entry.jsx"
+
+// export default function App() {
+
+//      const ninjaTurtles = ["Donatello", "Michaelangelo", "Rafael", "Leonardo"]
+//      const ninjaJSX = ninjaTurtles.map((ninja) => {
+//         return <h3>{ninja[0] + ninja.slice(1)}</h3>
+//      } )
+//     return (
+//         <>
+//             <Header />
+//             <main className="container">
+//                 {/* <Entry
+//                     img={{ 
+//                         src: "https://scrimba.com/links/travel-journal-japan-image-url",
+//                         alt: "Mount Fuji" 
+//                     }}
+//                     title="Mount Fuji"
+//                     country="Japan"
+//                     googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+//                     dates="12 Jan, 2021 - 24 Jan, 2021"
+//                     text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+//                 /> */}
+//                 {ninjaJSX}
+          
+
+//             </main>
+//         </>
+//     )
+// }
+
+// import jokesData from '../components/jokesData.js'
+
+
+// export default function App() {
+
+//     //receiveing the data which is an arra of objects and mapping it to joke components
+//     const jokesElement = jokesData.map((joke)=> {
+//         return <Jokes setup={joke.setup} punchline={joke.punchline} />
+//     })
+
+//     return (
+//         <>
+//         {jokesElement}
+//         </>
+        
+//     )
+// }
 
 export default function App() {
 
-     const ninjaTurtles = ["Donatello", "Michaelangelo", "Rafael", "Leonardo"]
-     const ninjaJSX = ninjaTurtles.map((ninja) => {
-        return <h3>{ninja[0] + ninja.slice(1)}</h3>
-     } )
-    return (
-        <>
-            <Header />
-            <main className="container">
-                {/* <Entry
-                    img={{ 
-                        src: "https://scrimba.com/links/travel-journal-japan-image-url",
-                        alt: "Mount Fuji" 
-                    }}
-                    title="Mount Fuji"
-                    country="Japan"
-                    googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-                /> */}
-                {ninjaJSX}
-          
+    //break down the data into entry components
+    const entryElements = data.map((entry) => {
+        return <Entry key={entry.id}
+                    img={entry.img}
+                    title={entry.title} 
+                    country={entry.country}
+                    googleMapsLink={entry.googleMapsLink}
+                    dates={entry.dates}
+                    text={entry.text}
+                    />
+    })
 
-            </main>
+    return (
+        //return entry components
+        <>
+        {entryElements}
         </>
     )
 }
