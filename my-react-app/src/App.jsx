ab7886a281110894aed919681a46b9e064334bda
Ochanda Charles <ochanda.charles.16@gmail.com>
2025-08-05 14:22:21 +0300

    reminded myself about mapping

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,11 +5,11 @@ import './App.css'
 import Jokes from '../components/jokes.jsx'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
+//   return (
+//     <>
       {/* <div className="contacts-container">
         <Contact 
         img=""
@@ -36,7 +36,7 @@ function App() {
         email="mrwizcaz@catnap.meow"
         />
       </div> */}
-      <div className="jokes-list">
+      {/* <div className="jokes-list">
 
 
         <Jokes
@@ -62,4 +62,37 @@ function App() {
     )
 }
 
-export default App
+export default App */}
+
+
+import Header from "../components/header.jsx"
+import Entry from "../components/entry.jsx"
+
+export default function App() {
+
+     const ninjaTurtles = ["Donatello", "Michaelangelo", "Rafael", "Leonardo"]
+     const ninjaJSX = ninjaTurtles.map((ninja) => {
+        return <h3>{ninja[0] + ninja.slice(1)}</h3>
+     } )
+    return (
+        <>
+            <Header />
+            <main className="container">
+                {/* <Entry
+                    img={{ 
+                        src: "https://scrimba.com/links/travel-journal-japan-image-url",
+                        alt: "Mount Fuji" 
+                    }}
+                    title="Mount Fuji"
+                    country="Japan"
+                    googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+                    dates="12 Jan, 2021 - 24 Jan, 2021"
+                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+                /> */}
+                {ninjaJSX}
+          
+
+            </main>
+        </>
+    )
+}
